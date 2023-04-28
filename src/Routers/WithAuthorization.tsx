4d610e3ca4ec9ba18971ce48a40efb3a8fedbf23
namtran02022003 <tranhuytunam02022003@gmail.com>
2023-04-28 08:47:17 +0700

    up code cho test

--- src/Routers/WithAuthorization.tsx
+++ src/Routers/WithAuthorization.tsx
@@ -1,17 +1,20 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function withAuthorization(WrappedComponent: FC) {
   function AuthorizedComponent(props: object) {
     const navigate = useNavigate()
+    const [flag, setFlag] = useState(false)
     useEffect(() => {
       const tokenLocal: null | string = localStorage.getItem('token') || ''
       if (!tokenLocal) {
         navigate('/login')
+      } else {
+        setFlag(true)
       }
     }, [navigate])
 
-    return <WrappedComponent {...props} />
+    return flag ? <WrappedComponent {...props} /> : ''
   }
 
   return AuthorizedComponent
